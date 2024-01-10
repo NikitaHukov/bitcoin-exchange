7e9257fe345a8be156e0017e894370026292f6af
Nikita <ngkhua2004@gmail.com>
2024-01-10 11:21:01 +0100

    dsadsa

---
+++ src/shared/components/Body.tsx
@@ -0,0 +1,14 @@
+export const Body = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          Inside Thailand
+          <div>
+            Buy, Sell Bitcoins <p>Simply & Securely</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
