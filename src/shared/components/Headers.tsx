2152cbe5ff37eea83d5047f54a0cc2fbc22308b1
Nikita <ngkhua2004@gmail.com>
2024-01-10 18:07:55 +0100

    fix:Header,margines

--- src/shared/components/Headers.tsx
+++ src/shared/components/Headers.tsx
@@ -2,9 +2,13 @@ export const Headers = () => {
   return (
     <div>
       <div className=" flex justify-between font-extrabold h-16 border border-gray-300 items-center">
-        <div className=" flex gap-2 ml-10">
-          Bitcoin Price:<div className="text-sky-500">43,106</div>{" "}
-          <div className="text-gray-400 ml-5s">5 March 2017 - 10:42pm</div>
+        <div className=" flex gap-6 pl-10">
+          <div>
+            <span>Bitcoin Price:</span>
+            <span className="text-sky-500"> 43,106</span>
+          </div>
+
+          <div className="text-gray-400 ">5 March 2017 - 10:42pm</div>
         </div>
         <div className="flex gap-4 items-center">
           <div>Trade</div>
@@ -17,21 +21,20 @@ export const Headers = () => {
             <div className="bg-yellow-500 h-5 w-5 rounded-lg"></div>
           </div>
 
-          <div className="flex gap-2 mr-10 items-center">
+          <div className="flex gap-2 pr-10 items-center">
             <div className="bg-yellow-500 h-5 w-5 rounded-lg"></div>Login
           </div>
         </div>
       </div>
       <div>
-        {" "}
-        <div className=" flex justify-between p-5">
-          <div className=" flex gap-5 text-4xl font-extrabold align-middle ml-11">
+        <div className=" flex justify-between p-5 border-b border-gray-300">
+          <div className=" flex gap-5 text-4xl font-extrabold align-middle pl-11">
             <div className="bg-yellow-500 h-10 w-10 rounded-lg"></div>Bitcoin
             <div className=" font-normal text-2xl p-1">
               Buy,Sell, Exchange Bitcoins in Thailand
             </div>
           </div>
-          <div className="flex gap-6 text-4xl font-extrabold mr-11">
+          <div className="flex gap-6 text-4xl font-extrabold pr-11">
             <div>Buy Bitcoin</div>
             <div>Sell Bitcoin</div>
             <div>Top-up</div>
